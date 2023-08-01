f7b6361273b0e7eae11deb43b384fa2ae457c56e
ManasviGoyal <mg.manasvi@gmail.com>
2023-08-01 14:12:44 +0200

    Create climbingStairs.cpp

---
+++ climbingStairs.cpp
@@ -0,0 +1,18 @@
+// Complexity - Time: O(n), Space: O(1)
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2) {
+            return n;
+        }
+
+        int first = 1, second =2, res = 0;
+        for(int i = 2; i < n; i++) {
+            res = first + second;
+            first = second;
+            second = res;
+        }
+        return res;
+    }
+};
